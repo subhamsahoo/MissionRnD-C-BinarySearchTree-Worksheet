81d8252e2f1fb2f519081704e248a77aafa9ae26
subhamsahoo <subhamsahoo@missionrnd.com>
2016-04-10 17:57:58 +0530

    Submit Commit

--- src/BSTTransversals.cpp
+++ src/BSTTransversals.cpp
@@ -23,13 +23,52 @@ struct node{
 };
 
 
-void inorder(struct node *root, int *arr){
-	
+int in = 0, pre = 0, post = 0;
+void inorderHelper(struct node *root, int *arr){
+	if (root != NULL)
+	{
+		inorderHelper(root->left, arr);
+		arr[in++] = root->data;
+		inorderHelper(root->right, arr);
+	}
+
 }
-void preorder(struct node *root, int *arr){
-	
+void inorder(struct node *root, int *arr)
+{
+	if (arr == NULL)
+		return;
+
+	in = 0;
+	inorderHelper(root, arr);
 }
-void postorder(struct node *root, int *arr){
-	
+void preorderHelper(struct node *root, int *arr){
+	if (root != NULL)
+	{
+		arr[pre++] = root->data;
+		preorderHelper(root->left, arr);
+		preorderHelper(root->right, arr);
+	}
+}
+void preorder(struct node *root, int *arr)
+{
+	if (arr == NULL)
+		return;
+	pre = 0;
+	preorderHelper(root, arr);
+}
+void postorderHelper(struct node *root, int *arr){
+	if (root != NULL)
+	{
+		postorderHelper(root->left, arr);
+		postorderHelper(root->right, arr);
+		arr[post++] = root->data;
+	}
+}
+void postorder(struct node *root, int *arr)
+{
+	if (arr == NULL)
+		return;
+	post = 0;
+	postorderHelper(root, arr);
 }
 
