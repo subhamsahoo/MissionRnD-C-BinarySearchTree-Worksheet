c8ea525855ccc663ba280046b0f5e422d5437fb8
subhamsahoo <subhamsahoo@missionrnd.com>
2016-04-28 22:36:33 +0530

    Submit Commit

--- src/BSTClosestLeafDistance.cpp
+++ src/BSTClosestLeafDistance.cpp
@@ -39,7 +39,68 @@ struct node{
   struct node *right;
 };
 
+int distBelow(struct node *root)
+{
+	int l = -1, r = -1;
+	if (root->left == NULL&&root->right == NULL)
+		return 0;
+	if (root->left != NULL)
+		l = distBelow(root->left);
+	if (root->right != NULL)
+		r = distBelow(root->right);
+	if (l == -1)
+	{
+		if (r != -1)
+			return  r + 1;
+		else return -1;
+	}
+	else if (r == -1)return l + 1;
+	else return l<r ? l + 1 : r + 1;
+}
+
+void find(struct node *root, int key, int *res, int *i, int *j)
+{
+	(*i)++;
+	struct node *prev;
+	int temp;
+	if (root == NULL)
+		return;
+	if (key == root->data)
+	{
+		*res = distBelow(root);
+		(*j) = (*i) - 1;
+	}
+	else if (key < root->data)
+	{
+		prev = root;
+		find(root->left, key, res, i, j);
+		if (*res != -1)
+		{
+			temp = distBelow(prev) + (*i) - (*j);
+			(*j)--;
+			if (*res>temp)*res = temp;
+		}
+	}
+	else
+	{
+
+		prev = root;
+		find(root->right, key, res, i, j);
+		if (*res != -1)
+		{
+			temp = distBelow(prev) + (*i) - (*j);
+			(*j)--;
+			if (*res > temp)*res = temp;
+		}
+	}
+}
 int get_closest_leaf_distance(struct node *root, struct node *temp)
 {
-  return -1;
+	int res = -1, i = 0, j = 0;
+	if (root == NULL || temp == NULL)return -1;
+	else
+	{
+		find(root, temp->data, &res, &i, &j);
+		return res;
+	}
 }
